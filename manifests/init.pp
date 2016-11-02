e0da8bb9937eb6d51055cb320ff5ed6ad4d06f79
Alwyn Kik <alwyn@firelay.com>
2016-11-02 13:04:56 +0100

    Updated puppet apt repository PGP public key

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,7 +25,7 @@ class puppet (
   apt::source { 'puppetlabs':
     location   => 'http://apt.puppetlabs.com',
     repos      => 'main',
-    key        => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+    key        => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
     key_server => 'pgp.mit.edu',
   } ->
   
